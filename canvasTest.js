d76ae969ff7bfa2aeec536cb3f33f3b846ba0216
negatron99 <negatron99@users.noreply.github.com>
2018-12-19 15:53:54 +0000

    Something like this

--- canvasTest.js
+++ canvasTest.js
@@ -1,52 +1,49 @@
 var canvas = document.getElementsByTagName("canvas")[0];
 var context = canvas.getContext('2d');
-var coordinates = {X:[],Y:[]}; 
-var drawing = new Boolean(false);
+ 
+var drawing = false;
+
 function dragStart(){
   	//console.log("mouse down");
 	if(drawing === true){
-		addCoor(event);
-		draw();
+		draw(event);
 	}else{
 		drawing = true;
-		addCoor(event);
 	}
 	
 }
 function drag(){
 	//console.log("mouse move");
 	if(drawing === true){
-		addCoor(event);
-		draw();
+		draw(event);
 	}
 	
 }
 function dragEnd(){
 	//console.log("mouse up");
 	if(drawing === true){
-		addCoor(event);
+		draw(event);
 		drawing = false;
 	}
-	draw();
-	coordinates.X.length =0;
-	coordinates.Y.length =0;
 }
 
-function addCoor(event){
-	coordinates.X.push(event.clientX-canvas.offsetLeft);
-	coordinates.Y.push(event.clientY-canvas.offsetTop);
-	//console.log(coordinates)
-}
 canvas.addEventListener("mousedown",dragStart);
 canvas.addEventListener("mousemove",drag);
 canvas.addEventListener("mouseup",dragEnd);
 
-function draw(){
-	context.beginPath();
-	for(let i=0;i<coordinates.X.length;i++){
-		context.lineTo(coordinates.X[i] , coordinates.Y[i]);
-		//console.log(coordinates.X[i] , coordinates.Y[i]);
-	}
-	context.strokeStyle ="green";
-	context.stroke();
+var lastX, lastY;
+function draw(event){
+	var X= event.clientX-canvas.offsetLeft,
+        Y = event.clientY-canvas.offsetTop;
+ 
+    	if (lastX) {
+        	context.beginPath();
+        	context.moveTo(lastX,lastY);
+        	context.lineTo( X,Y);
+		context.strokeStyle ="green";
+        	context.stroke();
+    	}
+
+    	lastX = X;
+    	lastY = Y;
 }
